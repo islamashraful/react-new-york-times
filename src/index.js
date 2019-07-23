d369c1c08e0e84a393ed51033adf13dae7be64ec
islamashraful <rx.ju.edu@gmail.com>
2019-07-23 20:13:27 +0600

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const element = document.getElementById("root");
 
 if (!element) throw new Error("Couldn't find element with id root");
 
-ReactDOM.render(<App />, element);
+ReactDOM.render(<p>The test app</p>, element);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
