f91f116d3a5334fbf782a78f726304600ab98fb4
islamashraful <rx.ju.edu@gmail.com>
2019-07-23 20:30:11 +0600

    Added boilerplate code for two screens

---
+++ src/app/screens/ArticleCardsContainerScreen/ArticleCardsContainerScreen.js
@@ -0,0 +1,13 @@
+// @flow
+
+import React, { PureComponent } from "react";
+
+type Props = {};
+
+class ArticleCardsContainerScreen extends PureComponent<Props> {
+  render() {
+    return <p>ArticleCardsContainerScreen</p>;
+  }
+}
+
+export default ArticleCardsContainerScreen;
---
+++ src/app/screens/ArticleDetailsScreen/ArticleDetailsScreen.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React, { PureComponent } from "react";
+
+type Props = {};
+
+/**
+ * Article Details Screen
+ * Show details of an article
+ */
+class ArticleDetailsScreen extends PureComponent<Props> {
+  render() {
+    return <p>Article Details</p>;
+  }
+}
+
+export default ArticleDetailsScreen;
